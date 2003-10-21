8ba981bf424cb64e11ac34d05794d93ad373fcb2
pelle <pelle>
2003-10-21 22:29:31 +0000

    Renamed NeudistException to NeuClearException and moved it to org.neuclear.commons where it makes more sense. Unhooked the XMLException in the xmlsig library from NeuClearException to make all of its exceptions an independent hierarchy. Obviously had to perform many changes throughout the code to support these changes.

---
+++ src/java/org/neuclear/commons/NeuClearException.java
@@ -0,0 +1,93 @@
+/*
+ * $Id: NeuClearException.java,v 1.1 2003/10/21 22:29:31 pelle Exp $
+ * $Log: NeuClearException.java,v $
+ * Revision 1.1  2003/10/21 22:29:31  pelle
+ * Renamed NeudistException to NeuClearException and moved it to org.neuclear.commons where it makes more sense.
+ * Unhooked the XMLException in the xmlsig library from NeuClearException to make all of its exceptions an independent hierarchy.
+ * Obviously had to perform many changes throughout the code to support these changes.
+ *
+ * Revision 1.2  2003/02/08 20:55:03  pelle
+ * Some documentation changes.
+ * Major reorganization of code. The code is slowly being cleaned up in such a way that we can
+ * get rid of the org.neudist.utils package and split out the org.neudist.xml.soap package.
+ * Got rid of tons of unnecessary dependencies.
+ *
+ * Revision 1.1  2003/01/18 18:12:30  pelle
+ * First Independent commit of the Independent XML-Signature API for NeuDist.
+ *
+ * Revision 1.4  2002/10/03 01:51:58  pelle
+ * Bunch of smaller fixes for bugs found during deployment.
+ * Also a bit more documentation.
+ * I'm happy with this being called rev. 0.4
+ *
+ * Revision 1.3  2002/09/21 23:11:16  pelle
+ * A bunch of clean ups. Got rid of as many hard coded URL's as I could.
+ *
+ * User: pelleb
+ * Date: 05-Jun-02
+ * Time: 13:05:46
+ * To change template for new class use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package org.neuclear.commons;
+
+
+
+public class NeuClearException extends Exception {
+    /**
+     * Constructs a new exception with <code>null</code> as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public NeuClearException() {
+    }
+
+    /**
+     * Constructs a new exception with the specified cause and a detail
+     * message of <tt>(cause==null ? null : cause.toString())</tt> (which
+     * typically contains the class and detail message of <tt>cause</tt>).
+     * This constructor is useful for exceptions that are little more than
+     * wrappers for other throwables (for example, {@link
+     * java.security.PrivilegedActionException}).
+     *
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method).  (A <tt>null</tt> value is
+     *         permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     * @since  1.4
+     */
+    public NeuClearException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param   message   the detail message. The detail message is saved for
+     *          later retrieval by the {@link #getMessage()} method.
+     */
+    public NeuClearException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message and
+     * cause.  <p>Note that the detail message associated with
+     * <code>cause</code> is <i>not</i> automatically incorporated in
+     * this exception's detail message.
+     *
+     * @param  message the detail message (which is saved for later retrieval
+     *         by the {@link #getMessage()} method).
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method).  (A <tt>null</tt> value is
+     *         permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     * @since  1.4
+     */
+    public NeuClearException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
